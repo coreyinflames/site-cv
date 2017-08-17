f92dc4043a165b843349f34a6705a2add9ac96f7
João Netto <joaonettopb@hotmail.com>
2017-08-17 18:21:23 -0300

    contact att
    contact att

--- contact.php
+++ contact.php
@@ -59,9 +59,9 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "First Name: ".clean_string($name)."\n";
+    $email_message .= "Nome: ".clean_string($name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Assunto: ".clean_string($comments)."\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
