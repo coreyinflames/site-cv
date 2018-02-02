995d23c59baf0f621676729443032887952c2511
Joao Netto <joaonettopb@hotmail.com>
2018-02-02 01:10:01 -0200

    commit backend

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@ app.controller('dadosFicha', function dadosFicha($scope) {
   $scope.nome = 'João Batista Mororó Netto'
   $scope.datas = '31/05/1991'
   $scope.nacionalidade = 'Brasileiro'
-  $scope.endereco = 'Av. Pombal, 189 - Manaira, João Pessoa - PB'
+  $scope.endereco = 'Av. Duarte de Azevedo, 640'
   $scope.email = 'fleyefilms@gmail.com'
   $scope.fone = '+55 (83) 99602-1359'
-  $scope.objetivos = 'Trabalhando como desenvolvedor, tenho como objetivo primário aprimorar meus conhecimentos em Front-end e iniciar o aprendizado em Back-end, tendo como objetivo final a preparação das minhas habilidades para tornar-me um desenvolvedor Fullstack.'
+  $scope.objetivos = 'Sou desenvolvedor back-end ASP.NET usando a linguagem C#, tambem tenho conhecimentos de front-end em HTML5, CSS3 e JQuery.'
   $scope.caracteristicas = 'Proativo, comunicativo, curioso, autodidata, inglês avançado'
   $scope.hobbies = 'Música, fotografia e esportes'
 
